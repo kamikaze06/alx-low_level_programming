53d961cfe9a83c653c79404c8fe0bc6d8355bf07
kamikaze06 <j.n.divin06@gmail.com>
2023-03-16 15:56:26 -0700

    hello puts task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-/* program that prints exactly \"Programming is like building a multilingual puzzle */
 int main(void)
+	/* print with puts */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
