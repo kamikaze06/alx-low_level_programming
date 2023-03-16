75768f33c4312a789ee292a68add57186b00d4b2
kamikaze06 <j.n.divin06@gmail.com>
2023-03-16 16:16:16 -0700

    hello puts task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-/** main - print with puts */
+/**
+ * main - Entry point for the program
+ *
+ * Print with puts
+ *
+ * Return: 0 on success, non-zero on failure
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
