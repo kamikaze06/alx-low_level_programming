4e791226bd727a4c36c5fc47b72820bd170003ad
kamikaze06 <j.n.divin06@gmail.com>
2023-03-17 16:19:59 -0700

    print alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * Print with printf
+ *
+ * Return: 0 on success, non-zero on failure
+ */
+int main(void)
+{
+	char i;
+	for(i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return	(0);
+}
