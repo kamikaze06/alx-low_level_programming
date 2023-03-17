7c319bfcd54a9f4951a2c7eec11ec81ca3c20c2b
kamikaze06 <j.n.divin06@gmail.com>
2023-03-16 18:20:37 -0700

    quote.c task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * Print exactly a sentence
+ *
+ * Return: 1 on success
+ */
+int	main(void)
+{
+	fflush(stdout);
+	dup2(fileno(stderr),	fileno(stdout));
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")
+return	(1);
+}
