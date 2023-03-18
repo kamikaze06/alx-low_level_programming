d0d533ac8b6f6b9f24419dad5151ead034cdf74e
kamikaze06 <j.n.divin06@gmail.com>
2023-03-17 17:58:18 -0700

    print comb task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,14 +14,12 @@ int main(void)
 	for	(i = 0; i <= 9; i++)
 	{
 		for	(j = 0; j <= 9; j++)
-			        if      (i == j)
-				{
-					continue;
-				}
+		{
 			putchar(i + '0');
 			putchar(j + '0');
 			putchar(',');
 			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return	(0);
