706a683cdfddd1302c11792f5690812f6a5054b5
kamikaze06 <j.n.divin06@gmail.com>
2023-03-21 13:34:36 -0700

    checks lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * int _islower(int c) -  checks for lowercase character
+ *
+ *  checks for lowercase character
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	int j = 97;
+	while (j <= 122)
+	{
+		return (1);
+	}
+}
