9a185ccc549bcfe6cf121d09492ea6fb12501ee9
kamikaze06 <j.n.divin06@gmail.com>
2023-03-21 09:40:13 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,17 +6,17 @@
  *
  * Return: On success 0 .
  */
-int main()
+int main(void)
 {
-        _putchar('_');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-        return (0);
+	return (0);
 }
 
