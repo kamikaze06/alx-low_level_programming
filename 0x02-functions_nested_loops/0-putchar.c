be20e976bddbada4732a32fecb78401d9f03847b
kamikaze06 <j.n.divin06@gmail.com>
2023-03-21 07:29:48 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include"main.h"
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -17,4 +18,3 @@ int main(void)
 	_putchar(char r'\n');
 	return (0);
 }
-#include"main.h"
