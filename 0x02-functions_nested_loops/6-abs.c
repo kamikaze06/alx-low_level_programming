daf2198b4e72dfc1c61da07669cbfd75c3e26ae5
kamikaze06 <j.n.divin06@gmail.com>
2023-03-21 14:48:17 -0700

    compute the absolute of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include"main.h"
+/**
+ * _abs - compute the absolute value of int
+ *
+ *
+ *
+ * Return: 0 
+ */
+int _abs(int)
+{
+	int i;
+
+	if (i < 0)
+	{
+		i = -1 * i;
+		_putchar('i');
+	}
+	else
+	{
+		_putchar('i');
+	}
+	return (0);
+}
