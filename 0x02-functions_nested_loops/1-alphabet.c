d18b50080d9228313e7b7368c5d4f1bb4aa8b42c
kamikaze06 <j.n.divin06@gmail.com>
2023-03-21 10:04:19 -0700

    _putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,11 @@
 #include"main.h"
+/**
+ * print_alphabet
+ *
+ * Print alphabet in lowercase using _putchar
+ *
+ * Return: 0 on success, non-zero on failure
+ */
 void print_alphabet(void)
 {
 	char i = 'a';
