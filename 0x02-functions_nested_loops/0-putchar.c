04575cc6434406fb67dce7eeb627062fe0ec763a
kamikaze06 <j.n.divin06@gmail.com>
2023-03-21 06:55:37 -0700

    putchar task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include<main.h>
+#include<stdio.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 0 .
+ */
+int main(void)
+{
+	int _putchar(char _);
+	int _putchar(char p);
+	int _putchar(char u);
+	int _putchar(char t);
+	int _putchar(char c);
+	int _putchar(char h);
+	int _putchar(char a);
+	int _putchar(char r);
+	return (0);
+}
