99cfdb65d73357181715b5faa508a1dad664d257
kamikaze06 <j.n.divin06@gmail.com>
2023-03-23 10:56:29 -0700

    print most numbers task

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,10 +8,17 @@ void print_most_numbers(void)
 	int a;
 
 	a = 0;
-	while (a <= 9 && (a != 2 || a != 4))
+	while (a <= 9 )
 	{
+		if (a == 2 || a == 4)
+		{
+			continue;
+		}
+		else
+		{
 		_putchar(a + 48);
 		a++;
+		}
 	}
 	_putchar('\n');
 }
