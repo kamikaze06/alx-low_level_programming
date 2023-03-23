13560612c8a9490bfec7e576dc049fd2d50aa7fd
kamikaze06 <j.n.divin06@gmail.com>
2023-03-23 10:52:10 -0700

    print most numbers task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * print_most_numbers - prints numbers from 0 to 9 except 2 and 4
+ *
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	a = 0;
+	while (a <= 9 && a != 2 && a != 4)
+	{
+		_putchar(a + 48);
+		a++;
+	}
+	_putchar('\n');
+}
