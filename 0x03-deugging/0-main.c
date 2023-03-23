beae3696c4d6b1c7743e565f26235bb546b12563
kamikaze06 <j.n.divin06@gmail.com>
2023-03-22 17:50:46 -0700

    multiple mains task

--- 0x03-deugging/0-main.c
+++ 0x03-deugging/0-main.c
@@ -6,10 +6,22 @@
  */
 int main(void)
 {
-	int i;
+        int n;
 
-	i = 0;
-	positive_or_negative(i);
-
-	return(0);
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        /* your code goes there */
+        if      (n > 0)
+        {
+                printf("%d is positive\n",      n);
+        }
+        else if (n == 0)
+        {
+                printf("%d is zero\n",  n);
+        }
+        else if (n < 0)
+        {
+                printf("%d is negative\n",      n);
+        }
+        return (0);
 }
