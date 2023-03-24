52c798d3c3db2e267bfb954a95999236f9a5b1e7
kamikaze06 <j.n.divin06@gmail.com>
2023-03-23 17:01:32 -0700

    print 10 times numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -17,10 +17,10 @@ void more_numbers(void)
 		{
 			if (j >= 10)
 			{
-				_putchar(j / 10 + 48);
+				_putchar((j / 10) + 48);
 				j++;
 			}
-			_putchar(j % 10 + 48);
+			_putchar((j % 10) + 48);
 			j++;
 		}
 		_putchar('\n');
