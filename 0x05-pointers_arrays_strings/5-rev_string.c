3bff780020a0f118d411e2ba9efcaf9d4143d2a1
kamikaze06 <j.n.divin06@gmail.com>
2023-03-27 12:37:29 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include"main.h"
+
+/**
+ * rev_string -  reverses a string
+ * @s - string
+ */
+
+void rev_string(char *s)
+{
+	int len;
+
+	len = 0;
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	s--;
+	while (len != 0)
+	{
+		_putchar(*s);
+		s--;
+		len--;
+	}
+	_putchar('\n');
+
+}
