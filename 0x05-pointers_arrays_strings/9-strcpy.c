5646910776a0a32dface41cc6c4ee059a4ca1720
kamikaze06 <j.n.divin06@gmail.com>
2023-03-28 10:22:04 -0700

    strcpy task

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * *_strcpy -  copies the string pointed to by src
+ * @dest: char
+ * @src: char
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a] != '\0'; a++)
+	{
+		desr[a] = src[a];
+	}
+	dest[a] = '\0';
+	return (dest);
+
+}
