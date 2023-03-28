fa848afd7de6e94266e6d32cb5d9a9c7f5239dc3
kamikaze06 <j.n.divin06@gmail.com>
2023-03-28 08:25:15 -0700

    puts2 task

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * puts2 -  prints every other character of a string
+ * @str: string
+ */
+
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str += 2;
+	}
+	_putchar('\n');
+}
