238ff89bb196a9edd0e0430bf947bf3a4ea3a08c
kamikaze06 <j.n.divin06@gmail.com>
2023-03-28 09:15:40 -0700

    puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include"main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: the string
+ */
+
+void puts_half(char *str)
+{
+	int len;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++
+	}
+	if (len % 2 != 0)
+	{
+		for (i = (len - 1) / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
+
