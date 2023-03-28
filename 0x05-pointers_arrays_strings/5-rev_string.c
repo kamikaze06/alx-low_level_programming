b1954f87b9acf990588e236c96d8fb5f4002980f
kamikaze06 <j.n.divin06@gmail.com>
2023-03-28 08:02:36 -0700

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,6 +9,7 @@ void rev_string(char *s)
 {
 	int len;
 	int i;
+	char tmp;
 
 	len = 0;
 	while (s[len] != '\0')
@@ -17,10 +18,8 @@ void rev_string(char *s)
 	}
 	for (i = 0; i < len / 2; i++)
 	{
-		char tmp = s[i];
+		tmp = s[i];
 		s[i] = s[len - i - 1];
 		s[len - i - 1] = tmp;
 	}
-	_putchar('\n');
-
 }
