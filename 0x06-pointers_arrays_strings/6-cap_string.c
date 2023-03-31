e172cb83b93035456e423fd15bfdbd7664acab2e
kamikaze06 <j.n.divin06@gmail.com>
2023-03-31 04:41:10 -0700

    cap string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include"main.h"
+
+/**
+ * *cap_string - capitalizes all words of a string
+ * @a: character
+ * Return: a 
+ */
+
+char *cap_string(char *a)
+{
+	int i;
+	int j;
+	char s[];
+
+	s[] = " \n\t,;.!?\"(){}";
+	if (a[0] >= 'a' && a[0] <= 'z')
+	{
+		a[0] -= 'a' - 'A';
+	}
+
+	for (i = 1; a[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 12; j++)
+		{
+			if (a[i - 1] == s[j])
+			{
+				a[i] -= 'a' - 'A';
+			}
+		}
+	}
+	return (a);
+}
