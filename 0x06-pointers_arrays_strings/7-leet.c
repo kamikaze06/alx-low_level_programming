a38489fcf23ff9993452d696349e2fe637a897cc
kamikaze06 <j.n.divin06@gmail.com>
2023-03-31 05:13:54 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,8 +10,8 @@ char *leet(char *a)
 {
 	int i;
 	int j;
-	char x[] = {a, A, e, E, o, O, t, T, l, L};
-	char z[] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
+	char x[] = "aAeEoOtTlL";
+	char z[] ="4433007711";
 
 	for (i = 0; a[i] != '\0';i++)
 	{
