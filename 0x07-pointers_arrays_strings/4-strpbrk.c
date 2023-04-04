31f4053018457c65dc55bee457523d7632e98285
kamikaze06 <j.n.divin06@gmail.com>
2023-04-03 18:05:09 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - searches a string for any of a set of bytes
+ *
+ * @s: pointer
+ * @accept: character
+ *
+ * Return: pointer to s or NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	char *ptr1 = s;
+
+	while (*s != '\0')
+	{
+		char *ptr2 = accept;
+
+		while (*ptr2 != '\0')
+		{
+			if (*s == *ptr2)
+			{
+				return (ptr1);
+			}
+			ptr2++;
+		}
+		s++;
+	}
+	return (NULL);
+}
