bd40229e802fd7dd8324f50558abdd0511305c98
kamikaze06 <j.n.divin06@gmail.com>
2023-04-09 11:28:53 -0700

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,26 +1,17 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * main - main function
+ * main -  prints the number of arguments passed into it
  * @argc: count
  * @argv: character
- * Return: int
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-	int count;
-	int digit;
-	
-	count = argc - 1;
+(void) argv;
+	printf("%d\n", argc - 1);
 
-	while (count > 0)
-	{
-		digit = count % 10;
-		_putchar(digit + '0');
-		count /= 10;
-	}
-
-	_putchar('\n');
 	return (0);
 }
