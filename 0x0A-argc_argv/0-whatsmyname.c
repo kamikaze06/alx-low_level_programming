9f3dbde0f7c959e100eac71c31bc5ab47dd09458
kamikaze06 <j.n.divin06@gmail.com>
2023-04-09 07:26:14 -0700

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,7 +10,22 @@
 int main(int argc, char *argv[])
 {
 	char *program_name = argv[argc - argc];
+	int i;
 
+	i = 0;
+	while (*program_name != '\0')
+	{
+		i++
+	}
+	i -= 1;
+	 while (i >= 0 && program_name[i] != '/' && program_name[i] != '\\')
+	 {
+		 i--;
+	 }
+	 if (i >= 0)
+	 {
+		 program_name = &program_name[i + 1];
+	 }
 	while (*program_name != '\0')
 	{
 		_putchar(*program_name);
