a4cec23224233bb07b3a2d57d02244fa54dc1da2
kamikaze06 <j.n.divin06@gmail.com>
2023-04-11 05:15:45 -0700

    str concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,11 +18,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		s1 = " ";
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		s2 = " ";
+		s2 = "";
 	}
 
 	concat = malloc((strlen(s1) + strlen(s1) + 1) * sizeof(char));
