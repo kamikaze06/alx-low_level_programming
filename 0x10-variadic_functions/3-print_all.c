3e9ba8d625ff30946db0591b080d7519e4c121cb
kamikaze06 <j.n.divin06@gmail.com>
2023-04-20 18:16:25 -0700

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -16,7 +16,7 @@ void print_all(const char * const format, ...)
 	{
 		while (format[i])
 		{
-			switch(format[i])
+			switch (format[i])
 			{
 				case 'c':
 					printf("%s%c", separator, va_arg(list, int));
