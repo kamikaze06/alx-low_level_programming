fde6251226ecdd7fa7c43dde1a6d7bcbe7b00ea2
kamikaze06 <j.n.divin06@gmail.com>
2023-05-04 07:54:37 -0700

    update task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,25 +13,21 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int uint = 0;
-	unsigned int i = 0;
-	int len = strlen(b);
-	const char *ptr = b + len - 1;
+	int i;
 
 	if (b == NULL)
 	{
 		return (0);
 	}
 
-	while (ptr >= b)
+	for (i = 0; b[i]; i++)
 	{
-		if (*ptr != '0' && *ptr != '1')
+		if (b[i] < '0' || b[i] > '1')
 		{
 			return (0);
 		}
 
-		uint += (*ptr - '0') * (1 << i);
-		ptr--;
-		i++;
+		uint = 2 * uint + (b[i] - '0');
 	}
 
 	return (uint);
